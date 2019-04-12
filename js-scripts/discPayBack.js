60e800e41d4faa2797b97be51af4258a59dc1ed1
Sergio <a01365942@itesm.mx>
2019-04-11 22:08:51 -0500

    Se agregaron comentarios a discPayBack

--- js-scripts/discPayBack.js
+++ js-scripts/discPayBack.js
@@ -1,6 +1,5 @@
 $(document).ready(function () 
 {
-    let num = 0;
     $('.nav li a').on('click', function(){//Cambiamos de tab
         $('.nav li a').removeClass("active");
         $(this).addClass("active");
@@ -11,7 +10,7 @@ $(document).ready(function ()
         $(tab).addClass('active');
     });
     $('#periodosID').on("keyup", function(e){//Leemos del campo de texto periodos
-        num = $('#periodosID').val();//Obtenemos el número de periodos
+        var num = $('#periodosID').val();//Obtenemos el número de periodos
         displayPeriodos(num);//Mostramos los campos en el DOM
     });
 
@@ -42,7 +41,10 @@ $(document).ready(function ()
         displayPeriodos(0);
     });   
 });
-
+/**
+ * @brief muestra en la GUI una tabla de 4*N celdas, siendo N el número de periodos
+ * @param numero_periodos es un número entero positivo que tiene significado por su propio nombre
+ */
 function displayPeriodos(numero_periodos){//Despliega filas de periodos con 3 columnas 
     var template;
 
@@ -64,12 +66,21 @@ function displayPeriodos(numero_periodos){//Despliega filas de periodos con 3 co
     $('#datosTabla').html(template);    
 }
 
+/**
+ * @brief muestra en la columna 'Cumulative Cash Flow' de la GUI el contenido del array cumCashFlow[]
+ * @param numero_periodos es un número entero positivo que tiene significado por su propio nombre
+ * @param cumCashFlow es un vector de números reales(+/-) que guarda la los valores de Cumulative Cash Flow de 1-n periodos
+ */
 function displayCumCashFlow(numero_periodos, cumCashFlow){
     for(var i = 1; i <= numero_periodos; i++){
         $(`#cumCash${i}`).val(cumCashFlow[i]);
     }
 }
-
+/**
+ * @brief obtiene los datos de la columna de 'outflows' de la GUI y lo convierte a un array
+ * @param numero_periodos es un número entero positivo que tiene significado por su propio nombre
+ * @return un arreglo de números reales(+/-) que contiene a los outflows de 1-n periodos
+ */
 function getOutflows(numero_periodos){
     var data = [];
 
@@ -78,7 +89,11 @@ function getOutflows(numero_periodos){
 
     return data;
 }
-
+/**
+ * @brief obtiene los datos de la columna de 'inflows' de la GUI y los guarda en un array
+ * @param numero_periodos es un número entero positivo que tiene significado por su propio nombre
+ * @return un arreglo de números reales que contiene los outflows de 1-n periodos
+ */
 function getInflows(numero_periodos){
     var data = [];
 
@@ -87,37 +102,53 @@ function getInflows(numero_periodos){
 
     return data;
 }
-function calculateNetCashFlow(periodos, inflows, outflows){//Calcula el flujo neto de caja de los N periodos
+/**
+ * @brief calcula el 'Net Cash Flow'
+ * @param numero_periodos es un número entero positivo que tiene significado por su propio nombre
+ * @param inflows es un array de números reales(+/-) que contiene los inflows de 1-n periodos
+ * @param outflows es un array de números reales(+/-) que contiene los outflows de 1-n periodos
+ * @return un array de númers reales(+/-) que contiene el 'Net cash Flow'
+ */
+function calculateNetCashFlow(numero_periodos, inflows, outflows){//Calcula el flujo neto de caja de los N periodos
     var netCash = [];
 
-    for(var i = 1; i <= periodos; i++)
-        netCash[i] = inflows[i] - outflows[i];
+    for(var i = 1; i <= numero_periodos; i++)
+        netCash[i] = inflows[i] - outflows[i]; 
 
     return netCash;
 }
-
-function calculateNPV(interes,periodos){//Calcula el Net Present Value, recibe 2 argumentos: interés y periodos
+/**
+ * @brief calcula el Net Present Value
+ * @param interes es un número de punto flotante sin la parte entera que representa la tasa de interés
+ * @param numero_periodos es un número entero positivo que tiene significado por su propio nombre
+ */
+function calculateNPV(interes,numero_periodos){//Calcula el Net Present Value, recibe 2 argumentos: interés y periodos
     var npv = [];
 
-    for(var n = 1; n <= periodos; n++)
+    for(var n = 1; n <= numero_periodos; n++)
         npv[n] =  1/(1+interes)**n
 
     return npv;
 }
 /**
- * 
- * @param {*} netCashFlow[] 
- * @param {*} npv[] 
- * @param {*} periodos:int 
+ * @brief calcula el 'Discounted Cash Flow'
+ * @param netCashFlow[] array de números reales(+/-) que guarda los valores del net cash flow de 1-n periodos
+ * @param npv[] array que guarda los valores del nvp de 1-n periodos
+ * @param numero_periodos es un número entero positivo que tiene significado por su propio nombre
  */
-function calculateDiscCashFlow(netCashFlow, npv, periodos){
+function calculateDiscCashFlow(netCashFlow, npv, numero_periodos){
     var discCashFlow = [];
-    for(var i = 1; i <= periodos; i++)
+    for(var i = 1; i <= numero_periodos; i++)
         discCashFlow[i] = netCashFlow[i] * npv[i];
 
     return discCashFlow;
 }
-
+/**
+ * @brief calcula el 'Cumulative Cash Flow'
+ * @param principal es un número entero positivo que representa la inversión inicial
+ * @param discCashFlow es un array de números reales(+/-) que guarda los valores del discounted cash flow de 1-n periodos 
+ * @param periodos es un número entero positivo que tiene significado por su propio nombre
+ */
 function calculateCumCashFlow(principal, discCashFlow, periodos){
     //Convertimos el argumento Principal en INT
     principal = parseInt(principal);
