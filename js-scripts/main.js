c8c70cfa76244ce1e3c1d1ca8717a28bc3858789
Sergio <a01365942@itesm.mx>
2019-04-14 10:40:10 -0500

    El código JS se ha modularizado con ayuda de una libreria externa
    Se utilizo la libreria: require-2.3.6.js
    Se integro solamente el módulo discPayBack.js
    Nota: Falta volver a comentar el código y organizarlo

--- js-scripts/main.js
+++ js-scripts/main.js
@@ -1,43 +1,59 @@
-$(document).ready(function () 
-{
-    $('.nav li a').on('click', function(){//Cambiamos de tab
-        $('.nav li a').removeClass("active");
-        $(this).addClass("active");
-        //Obtenemos el atributo href
-        var tab = $(this).attr('href');
-        //Ocultamos los otros tabs
-        $('.tab-content > div').removeClass('active');
-        $(tab).addClass('active');
-    });
-    $('#periodosID').on("keyup", function(e){//Leemos del campo de texto periodos
-        var num = $('#periodosID').val();//Obtenemos el número de periodos
-        displayPeriodos(num);//Mostramos los campos en el DOM
-    });
-
-    //Realizamos los calculos después de haber recibido los datos correspondientes
-    $('#bCalcular').on("click", function (e){
-        //Guardamos el número de periodos, la inversión inicial y la tasa de interés proporcionadas por el usuario
-        var periodos = $('#periodosID').val();
-        var principal = $('#principalID').val();
-        var interes = $('#interesID').val();
-        //1. Inflows y Outflows
-        var inflows = getInflows(periodos);//Obtenemos un array con los inflows
-        var outflows = getOutflows(periodos);//Obtenemos un array con los outflows
-        //2. Realizamos el calculo de net cash flow
-        var netCashFlow = calculateNetCashFlow(periodos, inflows, outflows);
-        //3. Calculamos el NPV
-        var npv = calculateNPV(interes, periodos);
-        //4. Calculamos el Discounted Cash Flow
-        var discCashFlow = calculateDiscCashFlow(netCashFlow, npv, periodos);
-        //5. Calculamos el cumulative Cash Flow
-        var cumCashFlow = calculateCumCashFlow(principal, discCashFlow, periodos);
-        //6. Mostramos el array de cumCashFlow en la GUI
-        displayCumCashFlow(periodos, cumCashFlow);
-    
-    });
-
-    $('#bLimpiar').on("click", function (e){//Limpiamos los campos y la información mostrada  
-        $('form').trigger('reset');
-        displayPeriodos(0);
-    });   
+require.config({
+    paths: {
+        'discPayBack' : './discPayBack',
+        'jquery' : '../frameworks/jquery-3.3.1.min',
+        'estilos' : '../css/estilos.css'
+    }
 });
+
+require(['discPayBack', 'jquery'], function(dpp, $)
+{
+    $(document).ready(function () 
+    {
+        $('.nav li a').on('click', function(){//Cambiamos de tab
+            $('.nav li a').removeClass("active");
+            $(this).addClass("active");
+            //Obtenemos el atributo href
+            var tab = $(this).attr('href');
+            //Ocultamos los otros tabs
+            $('.tab-content > div').removeClass('active');
+            $(tab).addClass('active');
+        });
+        $('#periodosID').on("keyup", function(e){//Leemos del campo de texto periodos
+            var num = $('#periodosID').val();//Obtenemos el número de periodos
+            dpp.displayPeriodos(num);//Mostramos los campos en el DOM
+        });
+
+        //Realizamos los calculos después de haber recibido los datos correspondientes
+        $('#bCalcular').on("click", function (e){
+            //Guardamos el número de periodos, la inversión inicial y la tasa de interés proporcionadas por el usuario
+            var periodos = $('#periodosID').val();
+            var principal = $('#principalID').val();
+            var interes = $('#interesID').val();
+            //1. Inflows y Outflows
+            var inflows = dpp.getInflows(periodos);//Obtenemos un array con los inflows
+            var outflows = dpp.getOutflows(periodos);//Obtenemos un array con los outflows
+            console.log(inflows, outflows);
+            //2. Realizamos el calculo de net cash flow
+            var netCashFlow = dpp.calculateNetCashFlow(periodos, inflows, outflows);
+            console.log("netCash: " + netCashFlow);
+            //3. Calculamos el NPV
+            var npv = dpp.calculateNPV(interes, periodos);
+            console.log("npv: " + npv);
+            //4. Calculamos el Discounted Cash Flow
+            var discCashFlow = dpp.calculateDiscCashFlow(netCashFlow, npv, periodos);
+            //5. Calculamos el cumulative Cash Flow
+            var cumCashFlow = dpp.calculateCumCashFlow(principal, discCashFlow, periodos);
+            //6. Mostramos el array de cumCashFlow en la GUI
+            dpp.displayCumCashFlow(periodos, cumCashFlow);
+        
+        });
+
+        $('#bLimpiar').on("click", function (e){//Limpiamos los campos y la información mostrada  
+            $('form').trigger('reset');
+            displayPeriodos(0);
+        });   
+    });
+
+});
+
