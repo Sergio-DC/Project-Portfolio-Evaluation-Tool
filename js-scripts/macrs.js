2f3275542eff415e9ac2c2abdd451ee21536ac25
Sergio <a01365942@itesm.mx>
2019-04-25 13:39:01 -0500

    El módulo de 'Depreciation' funciona parcialmente
    -Tengo dudas en lo respecto a los datos que se deben colocar en las
    columnas  'Dep Rate' y 'Tax per Year'
    -Falta que se mejoren algunos detalles como:
    	-El despliegue de la lista
    	-Programar a los campos para que validen de manera correcta

---
+++ js-scripts/macrs.js
@@ -0,0 +1,139 @@
+var MACRS_TABLE = {
+    '3_year': [0, .3333, .4445, .1481, .0741],
+    '5_year': [0, .2, .32, .1920, .1152, .1152, .0576],
+    '7_year': [0, .1429, .2449, .1749, .1249, .0893, .0892, .0893, .0446],
+    '10_year': [0, .1, .18, .1440, .1152, .0922, .0737, .0655, .0655, .0656, .0655, .0328],
+    '15_year': [0, .05,.0950,.0855,.0770,.0693,.0623,.0590,.0590,.0591,.0590,.0591,.0590,.0591,.0590,.0591,.0295],
+    '20_year': [0, .03750,.07219,.06677,.06177,.05713,.05285,.04888,.04522,.04462,.04461,.04462,.04461,.04462,.04461,.04462
+                ,.04461,.04462,.04461,.04462,.04461, .02231]
+}
+/**
+ * @brief muestra en la tabla del tab 1 una tabla de 4*N celdas, siendo N el número de periodos/filas
+ * @param {number} numero_periodos - es un número entero positivo que tiene significado por su propio nombre
+ */
+export function displayPeriodosTab3(numero_periodos){//Despliega filas de periodos con 3 columnas 
+    var template;
+    if(numero_periodos == "")
+        template = "";
+    else{
+        for(var i = 0; i < numero_periodos; i++)
+        {
+            template += `
+            <tr id="${i}ID">
+                <td>${i}</td>
+                <td><input id="years${i}" type="text" class="form-control-sm col-auto" style="width:80px;"></td>
+                <td><input id="depRate${i}" type="text" class="form-control-sm col-auto" style="width:120px;"></td>
+                <td><input id="AnnualDep${i}" type="text" class="form-control-sm col-auto" style="width:120px;"></td>
+                <td><input id="AccDep${i}" type="text" class="form-control-sm col-auto" style="width:120px;"></td>
+                <td><input id="valueInLedgers${i}" type="text" class="form-control-sm col-auto" style="width:120px;"></td>
+                <td><input id="taxPerYear${i}" type="text" class="form-control-auto" style="width:120px;"></td>
+            </tr>            
+            `;
+        }            
+    }
+    $('#datosTabla3').html(template);    
+}
+/**
+ * @brief función que calcula la 'depreciación'
+ * @param {Array<number>} macrs 
+ * @param {number} principal 
+ * @param {number} periodos
+ * @returns {Array<number>} un arreglo con los valores de la depreciaicón
+ */
+function calculateDep(macrs, principal, periodos){
+    var depreciation = [];
+    for(var i = 1; i <= periodos; i++){
+        depreciation[i] = macrs[i] * principal;
+    }
+    return depreciation;
+}
+/**
+ * @brief función que calcula la 'depreciación acumulada'
+ * @param {Array<number>} depreciation - array que contiene la depreciación
+ * @param {Array<number>} periodos - es un número entero positivo que tiene significado por su propio nombre
+ * @returns {Array<number>} array que contiene los valores de la 'Depreciación Acumulada'
+ */
+function calculateAccuDep(depreciation, periodos){
+    var accuDep = [];
+    for(var i = 1; i <= periodos; i++){
+        if(i == 1)
+            accuDep[i] = depreciation[i];
+        else
+            accuDep[i] = depreciation[i] + accuDep[i-1];
+    }
+    return accuDep;
+}
+
+/**
+ * 
+ * @param {Array<number>} depreciation - array que contiene la depreciación
+ * @param {number} principal
+ * @param {number} periodos - es un número entero positivo que tiene significado por su propio nombre
+ */
+function calculateValueInLedgers(depreciation, principal, periodos){  
+    var valueInLedgers = [];
+    valueInLedgers[0] = principal;
+
+    for(var i = 1; i < periodos; i++){
+        valueInLedgers[i] = valueInLedgers[i-1] - depreciation[i];
+    }
+    return valueInLedgers;
+}
+
+function displayYears(starting_year, periodos){
+    
+    for(var i = 0; i < periodos; i++){
+        $(`#years${i}`).val(starting_year++);
+    }
+    
+}
+
+function displayAnnualDep(depreciation, periodos){
+    for(var i = 0; i < periodos; i++)
+        $(`#AnnualDep${i}`).val(depreciation[i]);
+}
+
+function displayAccDep(accDep, periodos){
+    for(var i = 0; i < periodos; i++)
+        $(`#AccDep${i}`).val(accDep[i]);
+}
+
+function displayValueInLedgers(valor_libros, periodos){
+    for(var i = 0; i < periodos; i++)
+        $(`#valueInLedgers${i}`).val(valor_libros[i]);
+}
+
+export function runAlgorithm_MACRS(){
+    //Guardamos el número de periodos, la inversión inicial y la tasa de interés proporcionadas por el usuario
+    var periodos = $('#periodosID3').val();
+    var principal = $('#principalID3').val();
+    var tax = $('#taxID3').val();
+    var salvage_value = $('#svID3').val();
+    var starting_year = $('#start_year').val();
+    var period_salvage_value = $('#p_svID3').val();
+    var dep_category = $('#dep_category').val();
+    
+    console.log("Periodos: " + periodos);
+    console.log("Principal: " + principal);
+    console.log("Tax: " + tax);
+    console.log("salvage value: " + salvage_value);
+    console.log("startin year: " + starting_year);
+    console.log("Periodo Salvage Val: " + period_salvage_value);
+    console.log("Dep category " + dep_category);
+
+    //1. Identificar la categoria de MACRS
+    var macr_category = MACRS_TABLE[dep_category]
+    //2. Calcular la columna de Depreciación(MACRS*Principal)
+    var depreciation = calculateDep(macr_category, principal, periodos);
+    //3. Calcular la depreciación acumulada
+    var accDep = calculateAccuDep(depreciation, periodos);//arreglo que guarda la 'depreciación acumulada'
+    console.log("Depreciación Acumulada: " +accDep);
+    //4. Calcular el 'valor en libros'
+    var valor_libros = calculateValueInLedgers(depreciation, principal,periodos);
+    console.log("Valor en libros: " + valor_libros);
+    //5. Mostrar la información en la tabla
+    displayYears(starting_year, periodos);
+    displayAnnualDep(depreciation, periodos);
+    displayAccDep(accDep, periodos);
+    displayValueInLedgers(valor_libros, periodos);
+}
--- view/menu.php
+++ view/menu.php
@@ -172,59 +172,67 @@
                                 <p>Hola mundo</p>
                         </div><!--End Tab3-->
                         <div class="tab-pane bg-primary rounded border border-dark pt-4" id="tab4"><!--Start tab4: Depreciation-->
-                            <form action="" class="justify-content-center" id="form2">
+                            <form action="" class="justify-content-center" id="form3">
                                 <div class="form-group ml-3">
                                     <div class="row">
-                                        <label for="periodosID2" class="col-3"><p id="fontStyle">Periodos (drop list)</p></label>
+                                        <label for="periodosID3" class="col-3"><p id="fontStyle">Periodos (drop list)</p></label>
                                         <div class="col-2">
-                                            <input type="text" id="periodosID2" value="" class="form-control form-control-sm">
+                                            <input type="text" id="periodosID3" value="" class="form-control form-control-sm">
                                         </div>
-                                        <label for="svID" class="col-2"><p id="fontStyle">Starting Year</p></label>
+                                        <label for="start_year" class="col-2"><p id="fontStyle">Starting Year</p></label>
                                         <div class="col-2">
-                                            <input type="text" name="" id="svID" class="form-control form-control-sm" placeholder="">
+                                            <input type="text" name="" id="start_year" class="form-control form-control-sm" placeholder="">
                                         </div>
                                         <div class="col-2">
-                                            <button class="btn btn-outline-dark btn-sm" id="bCalcular2">Calcular Straight Line</button>
+                                            <button class="btn btn-outline-dark btn-sm" id="bStraightLine">Calcular Straight Line</button>
                                         </div>  
                                     </div>
                                     <div class="row">
-                                        <label for="principalID2" class="col-3"><p id="fontStyle">Principal</p></label>
+                                        <label for="principalID3" class="col-3"><p id="">Principal</p></label>
                                         <div class="col-2">
-                                            <input type="text" id="principalID2" value="" class="form-control form-control-sm">
+                                            <input type="text" id="principalID3" value="" class="form-control form-control-sm">
                                         </div>
-                                        <label for="principalID2" class="col-2"><p id="fontStyle">Period of S.V.</p></label>
+                                        <label for="p_svID3" class="col-2"><p id="fontStyle">Period of S.V.</p></label>
                                         <div class="col-2">
-                                            <input type="text" id="" value="" class="form-control form-control-sm">
-                                        </div>
-                                        <div class="col-2">
-                                            <button class="btn btn-outline-dark btn-sm" id="bLimpiar2">Calcular Macrs</button>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <label for="interesID2" class="col-3"><p id="fontStyle">Tax(%)</p></label>
-                                        <div class="col-2">
-                                            <input type="text" id="interesID2" value="" class="form-control form-control-sm">
-                                        </div>
-                                        <label for="svID" class="col-2"><p id="fontStyle">Depreciation Category</p></label>
-                                        <div class="col-2">
-                                            <input type="text" name="" id="svID" class="form-control form-control-sm" placeholder="">
+                                            <input type="text" id="p_svID3" value="" class="form-control form-control-sm">
                                         </div>
                                         <div class="col-2">
-                                            <button class="btn btn-outline-dark btn-sm" id="bLimpiar2">Limpiar Datos</button>
+                                            <button class="btn btn-outline-dark btn-sm" id="bMACRS">Calcular Macrs</button>
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <label for="taxID" class="col-3"><p id="fontStyle">Salvage Value</p></label>
+                                        <label for="taxID3" class="col-3"><p id="fontStyle">Tax(%)</p></label>
                                         <div class="col-2">
-                                            <input type="text" id="taxID" value="" class="form-control form-control-sm">
+                                            <input type="text" id="taxID3" value="" class="form-control form-control-sm">
+                                        </div>
+                                        <label for="dep_category" class="col-2"><p id="fontStyle">Depreciation Category</p></label>
+                                        <div class="col-2">
+                                            <select name="" id="dep_category" class="form-control selectpicker">
+                                                <option value="3_year">3-year</option>
+                                                <option value="5_year">5-year</option>
+                                                <option value="7_year">7-year</option>
+                                                <option value="10_year">10-year</option>
+                                                <option value="15_year">15-year</option>
+                                                <option value="20_year">20-year</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-2">
+                                            <button class="btn btn-outline-dark btn-sm" id="bLimpiar3">Limpiar Datos</button>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <label for="svID3" class="col-3"><p id="fontStyle">Salvage Value</p></label>
+                                        <div class="col-2">
+                                            <input type="text" id="svID3" value=" class="form-control form-control-sm">
                                         </div>
                                     </div>                                                                      
                                 </div>                    
                                                          
                                 <div class="row justify-content-center"><!--Centramos la tabla-->
-                                    <table class="table table-responsive-sm table-sm col-7 table-bordered"><!--Definition of the table-->
+                                    <table class="table table-responsive-sm table-sm col-6 table-bordered"><!--Definition of the table-->
                                         <thead class="thead-default">
                                             <tr>
+                                                <th>N</th>
                                                 <th>Years</th>
                                                 <th>Dep Rate</th>
                                                 <th>Annual Dep</th>
@@ -233,7 +241,7 @@
                                                 <th>Tax per year</th>
                                             </tr>
                                         </thead>
-                                        <tbody id="datosTabla2">
+                                        <tbody id="datosTabla3">
                                             
                                         </tbody>                            
                                     </table>
